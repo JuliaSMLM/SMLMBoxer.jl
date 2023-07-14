4310a533c8375535956fe5e0a17adb2d4e6dafb7
Documenter.jl <documenter@juliadocs.github.io>
2023-07-14 15:47:06 +0000

    build based on 766036f

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = SMLMBoxer","category":"page"},{"location":"#SMLMBoxer","page":"Home","title":"SMLMBoxer","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for SMLMBoxer.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [SMLMBoxer]","category":"page"}]
+}
